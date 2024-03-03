15d38fea629bdbc7948185256d84c596276b09f8
pengjiang80 <peng.jiang@hotmail.com>
2024-03-03 16:22:07 +0800

    update

--- modules/r53/variables.tf
+++ modules/r53/variables.tf
@@ -1,11 +1,7 @@
 variable "vpc_id" {
-  type = string
-  default = "vpc-0139b3c3e759664b8"
 }
 
 variable "route53_zone_name" {
-  type = string
-  default = "test.com"
 }
 
 variable "record_ips" {}
