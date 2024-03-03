d98f8ddaeaf90610b24188fce0a87bc7cdb61775
pengjiang80 <peng.jiang@hotmail.com>
2024-03-03 16:02:18 +0800

    update

--- modules/r53/main.tf
+++ modules/r53/main.tf
@@ -1,20 +1,25 @@
-locals {
-  name = var.record_ips.name
-  ips = toset([for i in var.record_ips.ips: i.ip ])
+data "aws_route53_zone" "existing" {
+  name = var.route53_zone_name
 }
 
 resource "aws_route53_zone" "private" {
-  name = var.route53_zone_name
+  count = data.aws_route53_zone.existing.zone_id != null ? 0 : 1
+  name  = var.route53_zone_name
 
   vpc {
     vpc_id = var.vpc_id
   }
 }
 
+locals {
+  zone_id = data.aws_route53_zone.existing.zone_id != null ? data.aws_route53_zone.existing.zone_id : aws_route53_zone.private[0].zone_id
+  name = var.record_ips.name
+  ips = toset([for i in var.record_ips.ips: i.ip ])
+}
 
 resource "aws_route53_record" "this" {
   for_each  = local.ips
-  zone_id = aws_route53_zone.private.zone_id
+  zone_id = local.zone_id
   name    = local.name
   type    = "A"
   ttl     = 60
