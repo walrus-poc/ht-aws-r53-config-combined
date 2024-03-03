b711f490bc5d00d9fcacf83ccece6c045e05b223
pengjiang80 <peng.jiang@hotmail.com>
2024-03-03 15:13:03 +0800

    update

--- modules/eventbridge/variables.tf
+++ modules/eventbridge/variables.tf
@@ -4,5 +4,8 @@ variable "lambda_arn" {
   default = "arn:aws:lambda:ap-northeast-1:831691747030:function:dns_health_check"
 }
 
-variable "record_ips" {
+variable "dns_name" { 
+}
+
+variable "ip_ports" {
 }
--- variables.tf
+++ variables.tf
@@ -1,18 +1,11 @@
 variable "record_ips" {
    type =  object({
      name = string
-     ips = list(map(string))
+     ips = list(map({
+       ip = string
+       port = string
+     }
+    ))
    }
    )
-}
-
-
-
-variable "ip_ports" {
-   type = list(
-    object({
-      ip  = string
-      port = string
-    })
-  )
 }
