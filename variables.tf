1fb11937ea355ee425968f2dbfa07f3053bb46ab
pengjiang80 <peng.jiang@hotmail.com>
2024-03-06 22:12:24 +0800

    update

--- variables.tf
+++ variables.tf
@@ -1,3 +1,32 @@
+#
+# Contextual Fields
+#
+
+variable "context" {
+  description = <<-EOF
+Receive contextual information. When Walrus deploys, Walrus will inject specific contextual information into this field.
+
+Examples:
+```
+context:
+  project:
+    name: string
+    id: string
+  environment:
+    name: string
+    id: string
+  resource:
+    name: string
+    id: string
+```
+EOF
+  type        = map(any)
+  default     = {}
+}
+
+# 
+# Others
+# 
 variable "route53_zone_name" {
   type = string
   default = "abc.com"
@@ -17,5 +46,4 @@ variable "record_ips" {
 variable "walrus_server_url" {
   type = string
   default = "https://walrus.sealio.net"
-  
 }
