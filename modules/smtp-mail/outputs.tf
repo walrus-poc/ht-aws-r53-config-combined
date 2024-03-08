f60ab18f877608fcc1daa64b720649e9cf66a473
pengjiang80 <peng.jiang@hotmail.com>
2024-03-08 11:44:56 +0800

    remove others

--- modules/smtp-mail/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "subject" {
-  description = "Rendered subject of the email"
-  value       = local.subject
-}
-
-output "body" {
-  description = "Rendered body of the email"
-  value       = local.body
-}
